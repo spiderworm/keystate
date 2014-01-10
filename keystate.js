814985f1f84deb95ddc799330312fd9143f75c59
Dan Prince <dan@codecurve.net>
2014-01-10 20:54:33 +0000

    Added a few extra key codes

--- keystate.js
+++ keystate.js
@@ -3,13 +3,20 @@
   
   states = {};
   keyMap = {
+    8: 'backspace',
+    9: 'tab',
+    13: 'enter',
+    17: 'ctrl',
+    18: 'alt',
+    20: 'caps',
+    27: 'esc',
     37: 'left',
+    32: 'space',
     38: 'up',
     39: 'right',
     40: 'down',
-    32: 'space',
-    13: 'enter',
-    9: 'tab'
+    45: 'insert',
+    46: 'delete'
   };
 
   function changeState(state) {
